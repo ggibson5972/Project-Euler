c9734b05b3ccf819dbc50ca4927f21b6c4ace4a1
Grace Gibson <30275636+ggibson5972@users.noreply.github.com>
2017-07-19 14:46:20 -0400

    Update euler_prob_1.py

--- euler_prob_1.py
+++ euler_prob_1.py
@@ -1,5 +1,8 @@
 result = 0
+#iterate from 1 to 1000
 for i in range(1000):
+    #determine if i is a multiple of 3 or 5
     if ((i % 3 == 0) or (i % 5 == 0)):
+        #if i is a multiple of 3 or 5, add to sum (result)
         result += i
 print result
